f37c08b3e64cc3272f2c1ae1eed143588e478ae5
vincentheynderickx <v.heynderickx@gmail.com>
2024-04-02 16:20:26 +0200

    create raw camenv

--- argentiere.py
+++ argentiere.py
@@ -2,8 +2,9 @@ import os, sys
 import numpy as np
 
 sys.path.append('PyTrx/')
-
 from CamEnv import CamEnv
 from Velocity import Velocity, Homography
 from FileHandler import writeHomogFile, writeVeloFile, writeVeloSHP, writeCalibFile
 from Utilities import plotVeloPX, plotVeloXYZ, interpolateHelper, plotInterpolate
+
+cameraenvironment = CamEnv('camdata.txt')
