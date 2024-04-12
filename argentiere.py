6d11f9dcdf11ae3dcd694bdb1403ac6679d277fd
Donatien de La Chesnais <donatien.houitte_de_la_chesnais@etu.minesparis.psl.eu>
2024-04-12 15:09:41 +0200

    Je rajoute l'image et un peu plus du début du code

--- argentiere.py
+++ argentiere.py
@@ -1,10 +1,28 @@
 import os, sys
 import numpy as np
 
+#import PyTrx modules
 sys.path.append('PyTrx/')
-from CamEnv import CamEnv
-from Velocity import Velocity, Homography
-from FileHandler import writeHomogFile, writeVeloFile, writeVeloSHP, writeCalibFile
-from Utilities import plotVeloPX, plotVeloXYZ, interpolateHelper, plotInterpolate
+from PyTrx.CamEnv import CamEnv
+from PyTrx.Velocity import Velocity, Homography
+from PyTrx.FileHandler import writeHomogFile, writeVeloFile, writeVeloSHP, writeCalibFile
+from PyTrx.Utilities import plotVeloPX, plotVeloXYZ, interpolateHelper, plotInterpolate
 
+
+#Define data output directory
+destination = '../Examples/results/test_velocity1/'
+
+
+#-----------------------   Create camera object   -----------------------------
+
+#Define camera environment
 cameraenvironment = CamEnv('camdata.txt')
+
+#Optimise camera environment to refine camera pose
+cameraenvironment.optimiseCamEnv('YPR')
+
+#Report camera data and show corrected image
+cameraenvironment.reportCamData()
+cameraenvironment.showGCPs()
+cameraenvironment.showPrincipalPoint()
+cameraenvironment.showResiduals()
